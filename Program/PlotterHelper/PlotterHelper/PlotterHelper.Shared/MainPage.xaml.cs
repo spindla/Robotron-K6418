f90af0a244ea0c70141aba3b409312a69ff0dd58
Alexander <spindler.alexander@outlook.com>
2022-08-13 20:24:08 +0200

    Added UNO Project for WinUI App and Web App

--- Program/PlotterHelper/PlotterHelper/PlotterHelper.Shared/MainPage.xaml.cs
+++ Program/PlotterHelper/PlotterHelper/PlotterHelper.Shared/MainPage.xaml.cs
@@ -13,24 +13,18 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace PlotterHelperGUI
+namespace PlotterHelper
 {
     /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainWindow : Window
+    public sealed partial class MainPage : Page
     {
-        public MainWindow()
+        public MainPage()
         {
             this.InitializeComponent();
-        }
-
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
         }
     }
 }
